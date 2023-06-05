6d231010436c22dbb1bd29ee62d1006148bfc4a5
adk <aknauf@gmail.com>
2023-06-05 13:07:39 +0400

    Bind to 0.0.0.0, instead of defaulting to 127.0.0.1

--- index.js
+++ index.js
@@ -65,11 +65,11 @@ fastify.delete('/tods/:resId', async (req, rsp) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0'})
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
   }
 }
-await start()
+start()
 
